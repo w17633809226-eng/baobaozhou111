2e4a8025e4ff8aa7a2d8d0c351a79a6ee385e30d
w17633809226-eng <w17633809226@gmail.com>
2026-03-24 11:34:58 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig, loadEnv} from 'vite';
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 3000,
+      hmr: process.env.DISABLE_HMR !== 'true',
+      proxy: {
+        '/alapi': {
+          target: 'https://v3.alapi.cn',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/alapi/, '')
+        }
+      }
+    },
+  };
+});
